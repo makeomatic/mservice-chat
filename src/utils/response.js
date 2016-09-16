3f63fc48b9d05f0ab6c75da2bec6e31a6ea09c01
AVVS <v@makeomatic.ru>
2016-09-16 20:02:56 +0300

    fix: pinnedAt normalization ?

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,4 +1,5 @@
 const omit = require('lodash/omit');
+const is = require('is');
 
 const TYPE_BAN = 'ban';
 const TYPE_MESSAGE = 'message';
@@ -7,16 +8,19 @@ const TYPE_ROOM = 'room';
 const TYPE_USER = 'user';
 
 function transform(object, type) {
-  const response = {
-    type,
-    attributes: omit(object.toJSON ? object.toJSON() : object, 'id'),
-  };
+  const response = { type };
 
   if (type === TYPE_PIN) {
-    response.id = `${object.roomId}.${object.pinnedAt.getTime()}`;
+    if (is.date(object.pinnedAt)) {
+      object.pinnedAt = object.pinnedAt.toISOString();
+    }
+
+    response.id = `${object.roomId}.${object.pinnedAt}`;
   } else if (object.id) {
     response.id = object.id;
   }
+
+  response.attributes = omit(is.fn(object.toJSON) ? object.toJSON() : object, 'id');
 
   return response;
 }
