8578c23d11e6d4ef7451fe2bb1d19fc807388af4
Aleksandr Sorokin <imarkoffko@gmail.com>
2016-12-19 13:26:48 +0300

    feat(rooms): broadcast action (#27)

---
+++ test/suites/internal.rooms.broadcast.js
@@ -0,0 +1,62 @@
+const assert = require('assert');
+const Chat = require('../../src');
+const { connect, emit } = require('../helpers/socketIO');
+const socketIOClient = require('socket.io-client');
+
+const chat = new Chat(global.SERVICES);
+
+describe('internal.rooms.broadcast', function suite() {
+  before('start up chat', () => chat.connect());
+
+  before('create room', () =>
+    chat.services.room
+      .create({ name: 'test room', createdBy: 'admin@foo.com' })
+      .tap((createdRoom) => {
+        this.room = createdRoom;
+        this.roomId = createdRoom.id.toString();
+      })
+  );
+
+  after('shutdown chat', () => chat.close());
+
+  it('should be able return error if request is not valid', () => {
+    const { amqp } = chat;
+
+    return amqp
+      .publishAndWait('chat.internal.rooms.broadcast', { roomId: this.roomId })
+      .reflect()
+      .then(inspection => inspection.error())
+      .then((response) => {
+        assert.equal(response.code, 400);
+        assert.equal(response.name, 'ValidationError');
+        assert.equal(response.message, 'internal.rooms.broadcast validation failed: data should'
+          + ' have required property \'event\', data should have required property \'message\'');
+      });
+  });
+
+  it('should be able to broadcast message', (done) => {
+    const { amqp } = chat;
+    const client = socketIOClient('http://0.0.0.0:3000');
+    const params = {
+      roomId: this.roomId,
+      event: 'testEvent',
+      message: {
+        foo: 'bar',
+      },
+    };
+
+    client.on(`broadcast.testEvent.${this.roomId}`, (message) => {
+      assert.equal(message.foo, 'bar');
+      client.disconnect();
+
+      setTimeout(done, 300);
+    });
+
+    connect(client)
+      .then(() => emit(client, 'chat.rooms.join', { id: this.roomId }))
+      .then(() => amqp.publishAndWait('chat.internal.rooms.broadcast', params))
+      .then((response) => {
+        assert.deepEqual(response, { meta: { status: 'success' } });
+      });
+  });
+});
