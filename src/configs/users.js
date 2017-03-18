bcce14f6fd97f1f3394c4ff6abeee56b34f1c5db
AVVS <v@makeomatic.ru>
2017-03-18 12:11:33 -0700

    fix: configs

--- src/configs/users.js
+++ src/configs/users.js
@@ -5,11 +5,13 @@ module.exports = {
       verify: 'verify',
       getMetadata: 'getMetadata',
       updateMetadata: 'updateMetadata',
+      list: 'list',
     },
     timeouts: {
       verify: 2000,
       getMetadata: 2000,
       updateMetadata: 2000,
+      list: 5000,
     },
     cache: {
       getMetadata: 30000,
