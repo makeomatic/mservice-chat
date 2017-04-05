ca6bc6335a5bb001f08c1830a2bda8e84609797e
Sasha Bratchikov <sashabratchikov@gmail.com>
2017-04-05 19:25:56 +0300

    room size (#35)

---
+++ src/actions/rooms.size.js
@@ -0,0 +1,30 @@
+/**
+* @api {http} <prefix>.rooms.size Get number of participants in room
+* @apiVersion 1.0.0
+* @apiName rooms.size
+* @apiGroup Rooms
+* @apiSchema {jsonschema=../../schemas/rooms.size.json} apiParam
+* @apiSchema {jsonschema=../../schemas/rooms.size.response.json} apiSuccess
+*/
+
+function RoomsSizeAction(request) {
+  const { params, socket } = request;
+  const roomId = params.roomId;
+  const room = socket.adapter.rooms[roomId];
+
+  return {
+    data: {
+      id: roomId,
+      type: 'room',
+      attributes: {
+        size: room.length,
+      },
+    },
+  };
+}
+
+RoomsSizeAction.auth = 'token';
+RoomsSizeAction.schema = 'rooms.size';
+RoomsSizeAction.transports = ['http', 'socketIO'];
+
+module.exports = RoomsSizeAction;
